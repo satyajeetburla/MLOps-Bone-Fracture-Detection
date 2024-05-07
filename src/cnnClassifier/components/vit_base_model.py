eda45297864ac4095ce9014713fec5bcd8cd1d94
Satyajeet Das <68110287+satyajeetburla@users.noreply.github.com>
2024-05-07 19:32:19 -0400

    update

---
+++ src/cnnClassifier/components/vit_base_model.py
@@ -0,0 +1,62 @@
+import os
+import tensorflow as tf
+from pathlib import Path
+from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
+        self.config = config
+
+    def get_base_model(self):
+        # Load the Vision Transformer model
+        self.model = tf.keras.applications.vit.ViT(
+            input_shape=self.config.params_image_size,
+            weights='imagenet21k+imagenet2012',  # Load weights pre-trained on ImageNet
+            include_top=False  # Do not include the top layer as we will add our own
+        )
+        self.save_model(path=self.config.base_model_path, model=self.model)
+
+    @staticmethod
+    def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                layer.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                layer.trainable = False
+
+        # Adding custom Top layers
+        flatten_in = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+        prediction = tf.keras.layers.Dense(
+            units=classes,
+            activation='softmax'
+        )(flatten_in)
+
+        full_model = tf.keras.models.Model(
+            inputs=model.input,
+            outputs=prediction
+        )
+
+        full_model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        full_model.summary()
+        return full_model
+
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+
+    @staticmethod
+    def save_model(path: Path, model: tf.keras.Model):
+        model.save(path)
